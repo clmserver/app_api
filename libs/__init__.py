92767306c4871d963313ceeea9e06ac74e0244df
LBelphgor <49678178+LBelphgor@users.noreply.github.com>
2019-06-29 17:56:48 +0800

    2019.6.29-lb

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,9 +1,8 @@
 from redis import Redis
 
-# r = Redis(host='localhost',
-#            port=6371, db=3)
 r = Redis(host='localhost',
-           port=6379, db=3)
+           port=6371, db=3)
+
 
 if __name__ == '__main__':
     print(r.keys("*"))
